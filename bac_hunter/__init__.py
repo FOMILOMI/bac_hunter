7195ddeb5772596b2334328ec836f4f28a3a01c3
FOMILOMI <ahmedavo70@gmail.com>
2025-08-25 05:17:06 +0300

    Delete bac_hunter directory

--- bac_hunter/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Lightweight package shim for tests and imports.
-
-Provides access to modules by delegating to top-level implementations.
-"""
-
--- bac_hunter/modes.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-# Delegate to top-level implementation to avoid duplication
-from modes import ModeProfile, get_mode_profile, DEFAULT_MODES  # noqa: F401
-
-__all__ = ["ModeProfile", "get_mode_profile", "DEFAULT_MODES"]
-
